c4f580c921ff52eb12b9046284d821bf2a01f8dd
ujjwal <ujjwalbaranwal221715@gmail.com>
2024-07-28 11:49:15 +0530

    basic is startiing of the [roject

---
+++ frontend/src/component/AppLayout.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+function AppLayout() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default AppLayout;
