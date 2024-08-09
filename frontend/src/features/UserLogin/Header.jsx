b52031369d766d6809add719bc5aafde2e047d22
Himanshuadv <hthimanshu739@gmail.com>
2024-08-09 22:55:47 +0530

    Added heading

--- frontend/src/features/UserLogin/Header.jsx
+++ frontend/src/features/UserLogin/Header.jsx
@@ -1,7 +1,10 @@
 function Header() {
   return (
-    <header className="bg-red-600 px-5 py-16 border border-red-700 border-solid">
-      This is the header
+    <header className=" px-4 py-10 ">
+     <div className="flex flex-col gap-2">
+        <h1 className="font-bold text-3xl font-heading ">Department Alumni Interaction and Interface Committee (DAIIC)</h1>
+        <h2 className="font-semibold text-xl font-heading">Welcome, Himanshu Tripathi</h2>
+     </div>
     </header>
   );
 }
